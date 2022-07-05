188e3a8f8695df21aa434a2db674d4a56e0a52d4
theabhayprajapati <theabhayprajapati@gmail.com>
2022-07-05 13:55:03 +0530

    mission complete

---
+++ app.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const { createFFmpeg, fetchFile } = require('@ffmpeg/ffmpeg');
+
+const ffmpeg = createFFmpeg({ log: true });
+
+(async () => {
+    await ffmpeg.load();
+    ffmpeg.FS('writeFile', '/video.mp4', await fetchFile('./video.mp4'));
+    //   await ffmpeg.run('-i', './video.mp4', 'test.mp4');
+    //   await fs.promises.writeFile('./test.mp4', ffmpeg.FS('readFile', 'test.mp4'));
+    await ffmpeg.run('-i', './video.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'out.gif')
+    const data = ffmpeg.FS('readFile', 'out.gif');
+    await fs.promises.writeFile('./out.gif', data);
+    
+    process.exit(0);
+
+})();
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,121 @@
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from 'react'
+
+// get video from user and use ffmpeg to convert it to gif
 
 const Home: NextPage = () => {
+  // video input
+  const [video, setVideo] = useState(null)
+  const [gif, setGif] = useState(null)
+  const ffmpeg = createFFmpeg({
+    log: true,
+  })
+  function takingVideo(arg0: File) {
+    console.log(arg0);
+    setVideo(arg0)
+  }
+  const startConverting = async () => {
+    try {
+      await ffmpeg.load();
+      ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
+
+      // Run the FFMpeg command
+      await ffmpeg.run('-i', 'test.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'out.gif');
+
+      // Read the result
+      const data = await ffmpeg.FS('readFile', 'out.gif');
+      // Create a URL
+      const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
+      setGif(url);
+      console.log(
+        'Conversion complete! You can find the result in the "out.gif" file. ⚡'
+      )
+    }
+    catch {
+      console.log('error')
+      console.log("FFMpeg couldn't convert the video to GIF.");
+    }
+  }
+
+  // ready
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Daily Tools</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
+      <main>
+        <div className="container mx-auto">
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold">Daily Tools</h1>
+            <p className="text-lg">
+              A tool for daily tools.
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <div className="flex flex-col items-center justify-center mt-4">
+            </div>
+          </div>
         </div>
-      </main>
+        <br />
+        <div className="container mx-auto">
+          {/* input */}
+          <div className="flex flex-col items-center justify-center">
+            <label className="text-lg">
+              <input
+                type="file"
+                accept="video/*"
+                onChange={(e) => {
+                  takingVideo(e.target.files[0])
+                }}
+              />
+            </label>
+            <br />
+            <button onClick={startConverting}>
+              Convert
+            </button>
+            {/* if videos is true then show video */}
+            {video && (
+              <div className="flex flex-col items-center justify-center">
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+                <video
+                  className="w-full h-full max-w-lg max-h-fit"
+                  controls
+                  src={URL.createObjectURL(video)}
+                />
+              </div>
+            )}
+          </div>
+        </div>
+        <br />
+        {/*show git  */}
+        {/* show gif here*/}
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center">
+            {
+              gif && (
+                <img
+                  src={gif}
+                  alt="gif"
+                  className="rounded-lg"
+                  width="300"
+                  height="300"
+                />
+              )
+            }
+            <br />
+            {/* download */}
+            <a href={gif} download="gif">
+              Download
+            </a>
+          </div>
+        </div>
+
+      </main>
     </div>
   )
 }
 
 export default Home
+
+
